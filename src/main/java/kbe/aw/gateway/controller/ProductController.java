8f178b13285c4b962655b205d3bacb8176cc7956
Artur <artur.horch@gmx.de>
2022-09-29 17:51:18 +0200

    KA-36 - adjust getAllProducts APi, so token can be send back

--- src/main/java/kbe/aw/gateway/controller/ProductController.java
+++ src/main/java/kbe/aw/gateway/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,7 @@ public class ProductController
    @Autowired
    private Validation validation;
 
-   @GetMapping("/products")
+   @PostMapping("/products")
    @Operation(summary = "get all Products")
    public List<Product> getAllProducts(@RequestBody String token)
    {
